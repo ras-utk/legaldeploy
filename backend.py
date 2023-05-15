22e0c0f64e43f9bcb770463844edde2b9fb95934
ras-utk <131155156+ras-utk@users.noreply.github.com>
2023-05-15 09:57:32 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -38,9 +38,9 @@ def exclude_id(obj):
 # Allow all origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://legaldeploy.onrender.com"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET","POST","PUT","DELETE"],
     allow_headers=["*"],
 )
 
